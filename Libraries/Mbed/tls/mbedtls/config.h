b624ef7aafbb8204338d54a8b70a5d4c28ee3d18
AzenkChina <azenk@localhost.localdomain>
2019-04-10 22:31:09 +0800

    Modifiy the config of mbedtls library to support ARM processor.

--- Libraries/Mbed/tls/mbedtls/config.h
+++ Libraries/Mbed/tls/mbedtls/config.h
@@ -58,7 +58,7 @@
  *
  * Comment to disable the use of assembly code.
  */
-#define MBEDTLS_HAVE_ASM
+//#define MBEDTLS_HAVE_ASM
 
 /**
  * \def MBEDTLS_NO_UDBL_DIVISION
@@ -1741,7 +1741,7 @@
  *
  * This modules adds support for the AES-NI instructions on x86-64
  */
-#define MBEDTLS_AESNI_C
+//#define MBEDTLS_AESNI_C
 
 /**
  * \def MBEDTLS_AES_C
@@ -2477,7 +2477,7 @@
  *
  * This modules adds support for the VIA PadLock on x86.
  */
-#define MBEDTLS_PADLOCK_C
+//#define MBEDTLS_PADLOCK_C
 
 /**
  * \def MBEDTLS_PEM_PARSE_C
